dba8d0a8ec86d77b634f8d85b8c1aac3fb8903f6
dbsdud00 <dbsdudsw@icloud.com>
2023-09-14 13:02:44 +0900

    search

--- flutter-012-book-api-v2/lib/main.dart
+++ flutter-012-book-api-v2/lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:book/api/naver_open_api.dart';
-import 'package:book/models/naver_book_dto.dart';
+import 'package:flutter/gestures.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import './api/naver_open_api.dart';
+import './models/naver_book_dto.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,9 +33,14 @@ class _ViewBooksPageState extends State<ViewBooksPage> {
   // Future<List<NaverBookDto>>
   Future<List<NaverBookDto>>? resultBooks = NaverOpenAPI().loadBooks();
 
-  void searchBooks(search) {
+  /// resultBooks state 변수를 변화시키는 함수
+  /// search 라는 매개변수에 전달된 값을 받아서
+  void searchBooks(String search) {
+    if (search.isEmpty) return;
+    // NaverOpenAPI().loadBooks() 에게 전달하고, 검색어에 해당하는 결과를 return 받기
     Future<List<NaverBookDto>>? searchResultBooks =
         NaverOpenAPI().loadBooks(search);
+    // return 받은 결과를 result Books state 변수에 반영하여 List 화면을 다시 그리기
     setState(() {
       resultBooks = searchResultBooks;
     });
@@ -72,7 +80,9 @@ class SearchBox extends StatelessWidget {
             child: TextField(
               controller: searchInputController,
               keyboardType: TextInputType.text,
+              // 입력 키보드에 검색 버튼이 나타나도록 설정하기
               textInputAction: TextInputAction.search,
+              // 키보드의 검색 버튼을 클릭했을 때 실행할 함수
               onSubmitted: (value) => searchBooks(value),
               decoration: const InputDecoration(
                 // border: OutlineInputBorder(),
@@ -98,23 +108,100 @@ class ViewListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: FutureBuilder(
-        future: resultBooks,
-        builder: (context, snapshot) {
-          return ListView.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) => ListTile(
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage(snapshot.data![index].image!),
+      /// Future<List<Dto>> type 의 데이터들을 사용하여
+      /// 화면에 보여질 Widget 을 생성하는 도구
+      ///
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+          future: resultBooks,
 
-                // child: Image.network(snapshot.data![index].image!),
+          /// FutureBuilder 가 resultBooks 데이더를 사용하여 List 데이터로 변환을 하고
+          /// 그 데이터를 다시 ListView.builder 에게 전달하여 실제로 화면에 보여질 리스트를 생성한다.
+          builder: (context, snapshot) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              // ListView.builder 는 리스트의 각 항목(한개 데이터)들을
+              // ListTile 에게 전달하여 한개 데이터를 표현할 View 를 만들고
+              // 그들을 모아서 리스트로 보여준다.
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: BookItemView(bookItem: snapshot.data![index]),
               ),
-              title: Text(snapshot.data![index].title!),
-              subtitle: Text(snapshot.data![index].author!),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
+    );
+  }
+}
+
+class BookItemView extends StatelessWidget {
+  const BookItemView({
+    super.key,
+    required this.bookItem,
+  });
+  final NaverBookDto bookItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      leading: Image.network(bookItem.image!),
+      title: Text(bookItem.title!),
+      subtitle: Text(bookItem.author!),
+      children: [
+        Image.network(bookItem.image!),
+        paddingText(
+          child: Text(
+            bookItem.title!,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        paddingText(
+          child: Text(
+            bookItem.description!,
+            style: const TextStyle(
+              fontSize: 15,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        paddingText(
+          child: RichText(
+            text: TextSpan(
+              text: "네이버 바로가기",
+              style: const TextStyle(
+                fontSize: 15,
+                color: Colors.blue,
+              ),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () => launchUrl(Uri.parse(bookItem.link!)),
+              // children: [
+              //   TextSpan(
+              //     text: "바로가기",
+              //     style: const TextStyle(
+              //       fontSize: 15,
+              //       color: Colors.blue,
+              //     ),
+              //     recognizer: TapGestureRecognizer()
+              //       ..onTap = () => launchUrl(Uri.parse(bookItem.link!)),
+              //   ),
+              // ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  // 함수로 Widget 을 분리한 후 return type 을 Widget 으로 변환하자
+  Widget paddingText({required Widget child}) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: child,
     );
   }
 }
--- flutter-012-book-api-v2/lib/models/naver_book_dto.dart
+++ flutter-012-book-api-v2/lib/models/naver_book_dto.dart
@@ -23,6 +23,9 @@ class NaverBookDto {
   }); //
 
   // jsonToDto(jsonToMap)
+  // 데이터를 CU(Create:insert, Update: Update)를 해야하는 경우
+  // 주로 NoSQL DB 에 저장할 때 Dto 데이터를 Map 형식으로 변환한다.
+  // Map 형식으로 변환된 데이터는 JSON type 으로 쉽게 변환이 가능하다.
   Map<String, dynamic> toMap() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data["title"] = title;
@@ -38,6 +41,7 @@ class NaverBookDto {
   }
 
   // DtoToJson(FromMap)
+  // openAPI 를 통하여 받은 JSON 데이터를 dto 객체로 변환시키는 method
   NaverBookDto.fromJson(Map<String, dynamic> json) {
     title = json["title"];
     link = json["link"];
